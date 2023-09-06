29870dabb8f8b871bb6db1f413b586b10c176de0
Hasan Kagan BAYRAM <hkhaanbayram@outlook.com>
2023-09-06 17:20:09 +0300

    Browser Router added. (react-router-dom)

--- Ui/noted/src/App.tsx
+++ Ui/noted/src/App.tsx
@@ -4,67 +4,17 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import { WeatherForecastService, OpenAPI } from './services/openapi';
 import UserPage from './features/user/user';
+import Main from './Main';
+import { Link } from 'react-router-dom';
 
 function App() {
-    OpenAPI.BASE = 'https://localhost:44317';
-    
-    const test = async () => {
-        const res = await WeatherForecastService.create({id : 1});
-        const rest = await WeatherForecastService.getWeatherForecast();
-        debugger;        
-    }
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <UserPage />
-                <button onClick={test}>
-                    Click Me
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <span>
-                    <span>Learn </span>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux-toolkit.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux Toolkit
-                    </a>
-                    ,<span> and </span>
-                    <a
-                        className="App-link"
-                        href="https://react-redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React Redux
-                    </a>
-                </span>
-            </header>
-        </div>
-    );
+        <>
+            <div>               
+                <Main />                
+            </div>
+        </>
+    )
 }
 
 export default App;
---
+++ Ui/noted/src/Main.tsx
@@ -0,0 +1,13 @@
+﻿import { Routes, Route } from 'react-router-dom';
+import { Counter } from './features/counter/Counter';
+import UserPage from './features/user/user';
+
+const Main = () => {
+    return (
+        <Routes>            
+            <Route path='/' element={<UserPage />} />
+            <Route path='/counters' element={<Counter />} />
+        </Routes>
+    );
+}
+export default Main;
